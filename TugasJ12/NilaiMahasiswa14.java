0eb52dcb267358285f28f3204e60bd586ca91a6d
Galih1004 <mochgalihputrapratama@gmail.com>
2025-12-01 20:03:52 +0700

    Tugas2 Commit

--- TugasJ12/NilaiMahasiswa14.java
+++ TugasJ12/NilaiMahasiswa14.java
@@ -1,13 +1,39 @@
 package TugasJ12;
 import java.util.Scanner;
 public class NilaiMahasiswa14{
-    static int isianArray ( int n) {
+    static void isianArray ( int... nilai) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Masukkan nilai mahasiswa ke-" + (n+1) + ": ");
-        int nilai = sc.nextInt();
-        return nilai;
+        for (int i = 0; i < nilai.length; i++) {
+         System.out.print("masukkan nilai mahasiswa ke-" + (i + 1) + " = ");
+        nilai[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+    static void tampilkanArray(int... nilai) {
+    System.out.println("\ndaftar nilai mahasiswa =");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("mahasiswa " + (i + 1) + " = " + nilai[i]);
+        }
+    }
+    static int totallz(int... nilai) {
+        int total = 0;
+        for (int n : nilai) {
+            total += n;
+        }
+        return total;
     }
 public static void main(String[] args) {
+    Scanner mok = new Scanner(System.in);
+            System.out.print("masukkan jumlah mahasiswa = ");
+        int jmlMhs = mok.nextInt();
 
+        int[] nilai = new int[jmlMhs];
+
+        isianArray(nilai);
+        tampilkanArray(nilai);
+        
+        int totalNilai = totallz(nilai);
+        System.out.println("\ntotal nilai mahasiswa = " + totalNilai);
+        mok.close();
     }
 }
